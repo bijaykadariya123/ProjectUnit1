3337d8a5d68539e0d3c7cf1c2a30651df865e1e1
Bijay Kadariya <bijaykadariya92@gmail.com>
2023-05-31 08:57:55 -0400

    update

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const $form = $("form");
-const $publish = $("publish");
+const $publish = $("#publish");
 
 $form.on("submit", (event) => {
-  //preventDefault() to prevent the refresh
   event.preventDefault();
-  // const formData = new FormData($form[0]);
   const formData = new FormData($form[0]);
-  // const countryName = formData.get("countryName");
   let monthNumber = parseInt(formData.get("month"));
   let dayNumber = parseInt(formData.get("day"));
-
   $.ajax(
     `https://holidays.abstractapi.com/v1/?api_key=bafcafa7fb2243929607c7d3481da810&country=US&year=2023&month=${monthNumber}&day=${dayNumber}`
   ).then((getDatas) => {
+    console.log(getDatas[0]);
+    console.log(getDatas[0].type);
+    const info = getDatas[0];
+    const div = $("#publish");
+    const newDiv = $("<div>").addClass("info1");
+
     if (
-      (+monthNumber === 1 && +dayNumber === 1) ||
+      (monthNumber === 1 && dayNumber === 1) ||
       (monthNumber === 1 && dayNumber === 16) ||
       (monthNumber === 2 && dayNumber === 20) ||
       (monthNumber === 5 && dayNumber === 29) ||
@@ -24,10 +26,16 @@ $form.on("submit", (event) => {
       (monthNumber === 11 && dayNumber === 10) ||
       (monthNumber === 11 && dayNumber === 23) ||
       (monthNumber === 12 && dayNumber === 25)
-    ) {
-      console.log(`Its a Holiday: ${getDatas[0].type}`);
-    } else {
-      console.log(`Its Not a Holiday`);
+    )
+      newDiv.html(`
+        <h1> It is a Holiday: ${info.type}</h1>
+        `);
+    else {
+      newDiv.html(`<h2> It is not a holiday: ${info.type} </h2>`);
     }
+
+    div.append(newDiv);
+    // const alphas = div.append(newDiv);
+    // console.log(alphas);
   });
 });
